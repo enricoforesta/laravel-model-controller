97cb8bca7ffa18c1a82033f252796e0d9e4a0743
enricoforesta <enrico.foresta97@gmail.com>
2024-01-29 14:56:04 +0100

    feat:scaffolding

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- assets con vite --}}
+    @vite('resources/js/app.js')
+    {{-- assets con vite --}}
+    <title>Laravel DC</title>
+</head>
+
+<body>
+    <h2 class="text-danger">ciao</h2>
+</body>
+
+</html>
